d2de0c34e30046c51452e7b4f26eb228b48bb27c
Anish Mathew Jose <anishjose247@gmail.com>
2024-02-02 15:42:02 +0530

    setup for production deployment

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 app = Flask(__name__)
-app.config['SECRET_KEY']='notagoodsecretkey'
+app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
 
 db=SQLAlchemy(app)
